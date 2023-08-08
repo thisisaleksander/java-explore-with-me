a0d317b5be1f25509d5cdb8970dc3da6a7d8d957
Alexander Sizov <work@alexander-sizov.ru>
2023-08-08 22:19:42 +0300

    stat_svc
    * add project structure
    
    * add classes
    
    * done statistics server
    
    * done statistics client
    
    * done docker
    
    * switch to java 11
    
    * renamed to stats-server
    
    * rafactored to java-11
    
    * add docker to statistics
    
    * refactor back
    
    * edited docker-compose.yml
    
    * edited docker-compose.yml
    
    * edited docker-compose.yml
    
    * edited docker-compose.yml
    
    * edited docker-compose.yml
    
    * edited docker-compose.yml
    
    * edited applicaton.properties
    
    * edited applicaton.properties

---
+++ statistics/statistics-server/src/main/java/ru/practicum/model/Statistics.java
@@ -0,0 +1,40 @@
+package ru.practicum.model;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Statistics", schema = "public")
+@Getter
+@Setter
+@Builder
+@ToString
+@AllArgsConstructor
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Statistics {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+
+    @NotNull
+    @Size(max = 100)
+    @Column(name = "app")
+    String app;
+
+    @NotNull
+    @Size(max = 250)
+    @Column(name = "uri")
+    String uri;
+
+    @NotNull
+    @Size(max = 15)
+    @Column(name = "ip")
+    String ip;
+
+    @Column(name = "time_added")
+    LocalDateTime created;
+}
