58dd6dee54832339924a81ba9de08c6851a70557
alexander <alexandersizov01@gmail.com>
2023-08-12 19:22:56 +0300

    fixed settings

--- statistics/statistics-server/src/main/java/ru/practicum/statistics/constants/Constants.java
+++ statistics/statistics-server/src/main/java/ru/practicum/statistics/constants/Constants.java
@@ -1,5 +1,5 @@
 package ru.practicum.statistics.constants;
 
 public class Constants {
-    public static final String STATISTICS_CLIENT_LINK = "http://stats-server:9090";
+    public static final String STATISTICS_CLIENT_LINK = "http://ewm-statistics-server:9090";
 }
